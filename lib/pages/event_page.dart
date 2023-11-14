873282497ea6c9ab1775e814004f5a96e4a5fdae
johncross00 <johncross.ktm@gmail.com>
2023-11-14 18:48:50 +0000

    Statefull widget

--- lib/pages/event_page.dart
+++ lib/pages/event_page.dart
@@ -1,8 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-class EventPage extends StatelessWidget {
+class EventPage extends StatefulWidget {
   const EventPage({super.key});
 
+  @override
+  State<EventPage> createState() => _EventPageState();
+}
+
+class _EventPageState extends State<EventPage> {
+
+  final events = [
+    {
+      "speaker": "Meet 1",
+      "subject": "This is the first meeting",
+      "avatar": "germain"
+    },
+    {
+      "speaker": "Meet 2",
+      "subject": "This is the second meeting",
+      "avatar": "apple"
+    },
+    {
+      "speaker": "Meet 3",
+      "subject": "This is the third meeting",
+      "avatar": "mac"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,14 +33,24 @@ class EventPage extends StatelessWidget {
         title: const Text('Event Page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset('assets/images/event.svg'),
-            const SizedBox(height: 20),
-            Text('Event Page'),
-          ],
-        ),
+          child: ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, index){
+              final event = events[index];
+              final avatar = event["avatar"];
+              final speaker = event["speaker"];
+              final subject = event["subject"];
+
+              return Card(
+                child: ListTile(
+                  leading: Image.asset("assets/images/$avatar.jpg"),
+                  title: Text("$speaker"),
+                  subtitle: Text("$subject"),
+                  trailing: Icon(Icons.more_vert),
+                ),
+              );
+            }
+          )
       ),
     );
   }
